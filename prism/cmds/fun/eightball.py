fcd1e9c2646f902afbdd5216401c2445384b63ee
iiPython <ben@iipython.cf>
2021-07-08 13:19:30 -0500

    Compress eightball responses

--- prism/cmds/fun/eightball.py
+++ prism/cmds/fun/eightball.py
@@ -12,27 +12,12 @@ class Eightball(commands.Cog):
         self.attr = {"name": "eightball", "desc": "Ask the magic eightball a question.", "cat": "fun", "usage": "eightball <question>"}
 
         self._8ball_responses = [
-            "It is certain.",
-            "It is decidedly so.",
-            "Without a doubt.",
-            "Yes definitely.",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful.",
-            "ur mom"
+            "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.",
+            "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.",
+            "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.",
+            "Better not tell you now", "Cannot predict now.", "Concentrate and ask again.",
+            "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.",
+            "Very doubtful.", "ur mom"
         ]
 
     @commands.command(pass_context = True, aliases = ["8ball"])
