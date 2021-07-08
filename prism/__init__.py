50196d28bf12b831d119ef5e29dcbc1b059b99d9
iiPython <ben@iipython.cf>
2021-07-08 18:05:43 -0500

    Bump version

--- prism/__init__.py
+++ prism/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "iiPython"
 __license__ = "MIT"
 __copyright__ = "(c) 2021 iiPython"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .core.bot import PrismBot
 from .utils.logging import logger
