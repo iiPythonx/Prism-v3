e26272129bff1b2232f55be76f16d71291f5fdef
iiPython <ben@iipython.cf>
2021-07-08 13:02:34 -0500

    New timer mechanism

--- prism/utils/timer.py
+++ prism/utils/timer.py
@@ -1,27 +1,34 @@
 # Copyright 2021 iiPython
 
 # Modules
+import secrets
+from typing import Union
 from datetime import datetime
+from secrets import token_hex
 
 # Timer class
 class Timer(object):
     def __init__(self) -> None:
         self._st_times = {}
+        self._ret_keys = {"s": lambda x: x, "ms": lambda x: float(x) * 1000}
 
-    def start(self, timer_name: str) -> None:
-        if timer_name in self._st_times:
-            raise RuntimeError("a timer with the name '{}' is already running".format(timer_name))
+    def start(self) -> str:
+        timer_id = token_hex(26)
+        self._st_times[timer_id] = datetime.now()
+        return timer_id
 
-        self._st_times[timer_name] = datetime.now()
+    def end(self, timer_id: str, return_as: str = "s", as_int: bool = False) -> Union[str, int]:
+        if timer_id not in self._st_times:
+            raise RuntimeError("invalid timer id: '{}'".format(timer_id))
 
-    def end(self, timer_name: str) -> str:
-        if timer_name not in self._st_times:
-            raise RuntimeError("no such timer: '{}'".format(timer_name))
+        st_time = self._st_times[timer_id]
+        del self._st_times[timer_id]
 
-        st_time = self._st_times[timer_name]
-        del self._st_times[timer_name]
+        # Handle return value
+        secs = round((datetime.now() - st_time).total_seconds(), 2)
+        vals = self._ret_keys[return_as](secs)
 
-        return str(round((datetime.now() - st_time).total_seconds(), 2))
+        return str(vals) if not as_int else int(round(vals))
 
 # Initialization
 timer = Timer()
