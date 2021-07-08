ed16617c468f8966e8da569ff6d4c29110f93ed7
iiPython <ben@iipython.cf>
2021-07-08 13:02:49 -0500

    Ping command

---
+++ prism/cmds/misc/ping.py
@@ -0,0 +1,31 @@
+# Copyright 2021 iiPython
+
+# Modules
+from discord.ext import commands
+from prism.utils.timer import timer
+
+# Command class
+class Ping(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.core = bot.core
+        self.attr = {"name": "ping", "desc": "Displays the bots ping time.", "cat": "misc", "usage": "ping"}
+
+    @commands.command(pass_context = True)
+    async def ping(self, ctx) -> any:
+
+        # Test API ping
+        tid = timer.start()
+        message = await ctx.send(embed = self.core.embed(description = "**Checking ping...**"))
+        api_ping = timer.end(tid, return_as = "ms", as_int = True)
+
+        # Test bot ping
+        bot_ping = round(self.bot.latency * 1000)
+
+        # Show ping
+        embed = self.core.embed(title = "Pong :ping_pong:", description = f"Bot: {bot_ping}ms | API: {api_ping}ms\nRoundtrip: {api_ping + bot_ping}ms")
+        return await message.edit(embed = embed)
+
+# Link
+def setup(bot) -> None:
+    return bot.add_cog(Ping(bot))
