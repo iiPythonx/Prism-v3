e7f319758e794ba7560acc9fd0123b4495a03aac
iiPython <ben@iipython.cf>
2021-07-11 19:47:17 -0500

    Basic inventory system

--- prism/utils/objects.py
+++ prism/utils/objects.py
@@ -20,31 +20,20 @@ class Inventory(object):
 
         items = {}
         for item in data:
-            items[item["name"]] = item["name"]
+            items[item["name"]] = item["amount"]
 
         return items
-
-    def _dump_items(self):
-        for item in self.items:
-            if not inventory_db.test_for([("name", item), ("userid", self.owner_id)]):
-                # create it
-                inventory_db.create((self.owner_id, item, self.items[item]))
-
-            else:
-                # update it
-                inventory_db.update({}, ())
-                # print("need to update item: " + item)
-
-        inventory_db.save()
 
     def add_item(self, name: str, amount: int = 1) -> None:
         if name in self.items:
             self.items[name] += amount
+            inventory_db.update({"amount": self.items[name]}, [("userid", self.owner_id), ("name", name)])
 
         else:
             self.items[name] = amount
+            inventory_db.create((self.owner_id, name, amount))
 
-        self._dump_items()
+        inventory_db.save()
 
 # Mapping
 map = {
