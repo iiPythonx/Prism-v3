0b3d38575e3550aa10e2a6d21b82b4e9f66dffb1
iiPython <ben@iipython.cf>
2021-11-19 12:51:34 -0600

    Moderation commands

---
+++ prism/cmds/mod/ban.py
@@ -0,0 +1,27 @@
+# Copyright 2021 iiPython
+
+# Modules
+import discord
+from discord.ext import commands
+from discord.commands import Option
+
+# Command class
+class Ban(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.core = bot.core
+
+    @commands.slash_command(description = "Bans a user from the server")
+    @commands.has_permissions(ban_members = True)
+    async def ban(self, ctx, user: Option(discord.Member, "The user to ban"), reason: Option(str, "Reason of ban", required = False, default = "None specified.")) -> any:
+        try:
+            await user.ban(reason = reason)
+
+        except Exception:
+            return await ctx.respond(embed = self.core.error("Missing permission to ban that user."))
+
+        return await ctx.respond(embed = self.core.small_embed(f"Successfully banned {user.name}."))
+
+# Link
+def setup(bot) -> None:
+    return bot.add_cog(Ban(bot))
---
+++ prism/cmds/mod/purge.py
@@ -0,0 +1,25 @@
+# Copyright 2021 iiPython
+
+# Modules
+import discord
+from discord.ext import commands
+from discord.commands import Option
+
+# Command class
+class Purge(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.core = bot.core
+
+    @commands.slash_command(description = "Clears the current channel.")
+    @commands.has_permissions(manage_messages = True)
+    async def purge(self, ctx, amount: Option(int, description = "The amount to clear")) -> any:
+        if amount > 100:
+            amount = 100
+
+        await ctx.channel.purge(limit = amount)
+        return await ctx.respond(embed = self.core.small_embed(f"Successfully cleared {amount} message(s)."), delete_after = 2)
+
+# Link
+def setup(bot) -> None:
+    return bot.add_cog(Purge(bot))
